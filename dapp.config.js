0a186e50abdca5d4e1f4e4cd7ccab9c23bbdd7eb
codingwithdidem <codingwithdidem@gmail.com>
2022-02-28 12:01:03 +0300

    prettierrc home page

---
+++ dapp.config.js
@@ -0,0 +1,6 @@
+const config = {
+  title: 'BoredApe Dapp',
+  description: 'A showcase dapp which is built for you to create your own'
+}
+
+export { config }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        coiny: ["Coiny", ...defaultTheme.fontFamily.sans],
+        coiny: ['Coiny', ...defaultTheme.fontFamily.sans]
       },
       colors: {
-        "brand-purple": "var(--clr-purple)",
-        "brand-pink": "var(--clr-pink)",
-        "brand-yellow": "var(--clr-yellow)",
-        "brand-blue": "var(--clr-blue)",
-        "brand-green": "var(--clr-green)",
-        "brand-background": "var(--clr-background)",
-      },
-    },
+        'brand-purple': 'var(--clr-purple)',
+        'brand-pink': 'var(--clr-pink)',
+        'brand-yellow': 'var(--clr-yellow)',
+        'brand-blue': 'var(--clr-blue)',
+        'brand-green': 'var(--clr-green)',
+        'brand-light': 'var(--clr-light)',
+        'brand-background': 'var(--clr-background)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
