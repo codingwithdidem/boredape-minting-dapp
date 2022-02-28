96f46d63d28cd0e299acfd1d066857edeca766bb
codingwithdidem <codingwithdidem@gmail.com>
2022-02-28 19:01:30 +0300

    create setmerkleroot script

---
+++ scripts/setMerkleRoot.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat')
+const { MerkleTree } = require('merkletreejs')
+const keccak256 = require('keccak256')
+const whitelist = require('./whitelist.js')
+
+const BASE_URI = 'ipfs://Qmb5A1fFECM2iFHgUioii2khT814nCi6VU9aHXHHqNxHCK/'
+const proxyRegistryAddressRinkeby = '0xf57b2c51ded3a29e6891aba85459d600256cf317'
+const proxyRegistryAddressMainnet = '0xa5409ec958c83c3f309868babaca7c86dcb077c1'
+
+async function main() {
+  const nftFactory = await hre.ethers.getContractFactory('BoredApe')
+  const nftContract = await nftFactory.attach(
+    '0x0312e42e4b55823a3C41769DC0B07F382dECc247'
+  )
+
+  // Calculate merkle root from the whitelist array
+  const leafNodes = whitelist.map((addr) => keccak256(addr))
+  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
+  const root = merkleTree.getRoot()
+
+  await nftContract.setWhitelistRoot(root)
+
+  console.log('Whitelist root set to:', root)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
