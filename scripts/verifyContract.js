f8f639b9cabcd96f01c198cdc8a9eec144422122
codingwithdidem <codingwithdidem@gmail.com>
2022-02-28 18:58:51 +0300

    verify contract

---
+++ scripts/verifyContract.js
@@ -0,0 +1,30 @@
+require('@nomiclabs/hardhat-etherscan')
+const hre = require('hardhat')
+const { MerkleTree } = require('merkletreejs')
+const keccak256 = require('keccak256')
+const whitelist = require('./whitelist.js')
+
+const BASE_URI = 'ipfs://Qmb5A1fFECM2iFHgUioii2khT814nCi6VU9aHXHHqNxHCK/'
+const proxyRegistryAddressRinkeby = '0xf57b2c51ded3a29e6891aba85459d600256cf317'
+const proxyRegistryAddressMainnet = '0xa5409ec958c83c3f309868babaca7c86dcb077c1'
+
+async function main() {
+  // Calculate merkle root from the whitelist array
+  const leafNodes = whitelist.map((addr) => keccak256(addr))
+  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
+  const root = merkleTree.getRoot()
+
+  await hre.run('verify:verify', {
+    address: '0x0312e42e4b55823a3C41769DC0B07F382dECc247',
+    constructorArguments: [BASE_URI, root, proxyRegistryAddressRinkeby]
+  })
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
