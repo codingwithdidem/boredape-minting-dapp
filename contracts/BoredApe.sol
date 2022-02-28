fbcb682f087c8ea98361e328e9d8bbbce5948e32
codingwithdidem <codingwithdidem@gmail.com>
2022-02-28 18:25:35 +0300

    create boredape contract

---
+++ contracts/BoredApe.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import '@openzeppelin/contracts/finance/PaymentSplitter.sol';
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// @author codingwithdidem
+// @contact codingwithdidem@gmail.com
+
+contract BoredApe is 
+    ERC721, 
+    Ownable, 
+    ReentrancyGuard, 
+    PaymentSplitter 
+{
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+
+    bytes32 public root;
+    
+    address proxyRegistryAddress;
+
+    uint256 public maxSupply = 100;
+
+    string public baseURI; 
+    string public notRevealedUri = "ipfs://QmYUuwLoiRb8woXwJCCsr1gvbr8E21KuxRtmVBmnH1tZz7/hidden.json";
+    string public baseExtension = ".json";
+
+    bool public paused = false;
+    bool public revealed = false;
+    bool public presaleM = false;
+    bool public publicM = false;
+
+    uint256 presaleAmountLimit = 3;
+    mapping(address => uint256) public _presaleClaimed;
+
+    uint256 _price = 10000000000000000; // 0.01 ETH
+
+    Counters.Counter private _tokenIds;
+
+    uint256[] private _teamShares = [25, 35, 40]; // 3 PEOPLE IN THE TEAM
+    address[] private _team = [
+        0x933572D5F83B00A998102b7bf1a99c0f197E685B, // Admin Account gets 25% of the total revenue
+        0x82de9CE4a49fFeC4C41Cf733126F618eD83a879C, // Test Account gets 35% of the total revenue
+        0x8a7aC9834e2D4487Da22Dc130C97Ee8fBDc85568 // VIP Account gets 40% of the total revenue
+    ];
+
+    constructor(string memory uri, bytes32 merkleroot, address _proxyRegistryAddress)
+        ERC721("BoredApe", "APE")
+        PaymentSplitter(_team, _teamShares) // Split the payment based on the teamshares percentages
+        ReentrancyGuard() // A modifier that can prevent reentrancy during certain functions
+    {
+        root = merkleroot;
+        proxyRegistryAddress = _proxyRegistryAddress;
+
+        setBaseURI(uri);
+    }
+
+    function setBaseURI(string memory _tokenBaseURI) public onlyOwner {
+        baseURI = _tokenBaseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function reveal() public onlyOwner {
+        revealed = true;
+    }
+
+    function setMerkleRoot(bytes32 merkleroot) 
+    onlyOwner 
+    public 
+    {
+        root = merkleroot;
+    }
+
+    modifier onlyAccounts () {
+        require(msg.sender == tx.origin, "Not allowed origin");
+        _;
+    }
+
+    modifier isValidMerkleProof(bytes32[] calldata _proof) {
+         require(MerkleProof.verify(
+            _proof,
+            root,
+            keccak256(abi.encodePacked(msg.sender))
+            ) == true, "Not allowed origin");
+        _;
+   }
+
+    function togglePause() public onlyOwner {
+        paused = !paused;
+    }
+
+    function togglePresale() public onlyOwner {
+        presaleM = !presaleM;
+    }
+
+    function togglePublicSale() public onlyOwner {
+        publicM = !publicM;
+    }
+
+
+    function presaleMint(address account, uint256 _amount, bytes32[] calldata _proof)
+    external
+    payable
+    isValidMerkleProof(_proof)
+    onlyAccounts
+    {
+        require(msg.sender == account,          "CryptoPunks: Not allowed");
+        require(presaleM,                       "CryptoPunks: Presale is OFF");
+        require(!paused,                        "CryptoPunks: Contract is paused");
+        require(
+            _amount <= presaleAmountLimit,      "CryptoPunks: You can't mint so much tokens");
+        require(
+            _presaleClaimed[msg.sender] + _amount <= presaleAmountLimit,  "CryptoPunks: You can't mint so much tokens");
+
+
+        uint current = _tokenIds.current();
+
+        require(
+            current + _amount <= maxSupply,
+            "CryptoPunks: max supply exceeded"
+        );
+        require(
+            _price * _amount <= msg.value,
+            "CryptoPunks: Not enough ethers sent"
+        );
+             
+        _presaleClaimed[msg.sender] += _amount;
+
+        for (uint i = 0; i < _amount; i++) {
+            mintInternal();
+        }
+    }
+
+    function publicSaleMint(uint256 _amount) 
+    external 
+    payable
+    onlyAccounts
+    {
+        require(publicM,                        "CryptoPunks: PublicSale is OFF");
+        require(!paused, "CryptoPunks: Contract is paused");
+        require(_amount > 0, "CryptoPunks: zero amount");
+
+        uint current = _tokenIds.current();
+
+        require(
+            current + _amount <= maxSupply,
+            "CryptoPunks: Max supply exceeded"
+        );
+        require(
+            _price * _amount <= msg.value,
+            "CryptoPunks: Not enough ethers sent"
+        );
+        
+        
+        for (uint i = 0; i < _amount; i++) {
+            mintInternal();
+        }
+    }
+
+    function mintInternal() internal nonReentrant {
+        _tokenIds.increment();
+
+        uint256 tokenId = _tokenIds.current();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        if (revealed == false) {
+            return notRevealedUri;
+        }
+
+        string memory currentBaseURI = _baseURI();
+    
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+        notRevealedUri = _notRevealedURI;
+    }
+
+    function totalSupply() public view returns (uint) {
+        return _tokenIds.current();
+    }
+
+    /**
+     * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator)
+        override
+        public
+        view
+        returns (bool)
+    {
+        // Whitelist OpenSea proxy contract for easy trading.
+        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+        if (address(proxyRegistry.proxies(owner)) == operator) {
+            return true;
+        }
+
+        return super.isApprovedForAll(owner, operator);
+    }
+}
+
+
+
+/**
+  @title An OpenSea delegate proxy contract which we include for whitelisting.
+  @author OpenSea
+*/
+contract OwnableDelegateProxy {}
+
+/**
+  @title An OpenSea proxy registry contract which we include for whitelisting.
+  @author OpenSea
+*/
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+
+
